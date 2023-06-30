3d554131e0cd549dfbb5814897f5b813dfee43ca
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-06-30 16:41:29 +0900

    fix PunishingBirdEntity

--- src/main/java/constantan/lobotomy/common/entity/PunishingBirdEntity.java
+++ src/main/java/constantan/lobotomy/common/entity/PunishingBirdEntity.java
@@ -1,5 +1,7 @@
 package constantan.lobotomy.common.entity;
 
+import constantan.lobotomy.lib.LibDebug;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.network.syncher.EntityDataAccessor;
@@ -11,12 +13,16 @@ import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.control.FlyingMoveControl;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.pathfinder.Path;
+import net.minecraft.world.phys.AABB;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -38,7 +44,6 @@ public class PunishingBirdEntity extends Monster implements IAnimatable {
 
     protected static final AnimationBuilder IDLE_NORMAL = new AnimationBuilder()
             .addAnimation("animation.punishing_bird.idle_normal", ILoopType.EDefaultLoopTypes.LOOP);
-
     protected static final AnimationBuilder FLY_NORMAL = new AnimationBuilder()
             .addAnimation("animation.punishing_bird.fly_normal", ILoopType.EDefaultLoopTypes.LOOP);
     protected static final AnimationBuilder ATTACK_NORMAL = new AnimationBuilder()
@@ -48,13 +53,12 @@ public class PunishingBirdEntity extends Monster implements IAnimatable {
     protected static final AnimationBuilder ATTACK_ANGRY = new AnimationBuilder()
             .addAnimation("animation.punishing_bird.attack_angry", ILoopType.EDefaultLoopTypes.PLAY_ONCE);
 
-    public static final float NORMAL_MAX_HEALTH = 200.0f;
-    public static final float ANGRY_MAX_HEALTH = 1000.0f;
-
-    private boolean angry = false;
+    public static final float NORMAL_MAX_HEALTH = 200.0F;
+    public static final float ANGRY_MAX_HEALTH = 1000.0F;
 
     public PunishingBirdEntity(EntityType<? extends Monster> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
+        this.moveControl = new FlyingMoveControl(this, 10, false);
     }
 
     @Override
@@ -70,27 +74,43 @@ public class PunishingBirdEntity extends Monster implements IAnimatable {
         return flag;
     }
 
+    @Override
+    protected PathNavigation createNavigation(Level pLevel) {
+        FlyingPathNavigation flyingpathnavigation = new FlyingPathNavigation(this, pLevel);
+        flyingpathnavigation.setCanOpenDoors(false);
+        flyingpathnavigation.setCanFloat(true);
+        flyingpathnavigation.setCanPassDoors(true);
+        return flyingpathnavigation;
+    }
+
     public static AttributeSupplier setAttributes() {
         return Monster.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, NORMAL_MAX_HEALTH)
-                .add(Attributes.ATTACK_DAMAGE, 10.0f)
-                .add(Attributes.ATTACK_SPEED, 1.0f)
-                .add(Attributes.MOVEMENT_SPEED, 0.1f)
-                .add(Attributes.FLYING_SPEED, 0.4f).build();
+                .add(Attributes.ATTACK_DAMAGE, 10.0F)
+                .add(Attributes.ATTACK_SPEED, 1.0F)
+                .add(Attributes.MOVEMENT_SPEED, 0.1F)
+                .add(Attributes.FLYING_SPEED, 0.3F)
+                .add(Attributes.KNOCKBACK_RESISTANCE, 0.5F).build();
     }
 
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(1, new FloatGoal(this));
-        this.goalSelector.addGoal(2, new PunishingBirdAngryAttackGoal(this, 2.0f, false));
-        this.goalSelector.addGoal(3, new PunishingBirdNormalAttackGoal(this, 1.0f, false));
-        this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+        this.goalSelector.addGoal(2, new PunishingBirdAngryAttackGoal(this, 2.0F, false));
+        this.goalSelector.addGoal(3, new PunishingBirdNormalAttackGoal(this, 1.0F, false));
+        this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1.0F));
         this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
 
-        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true, true));
-        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(
-                this, LivingEntity.class, 10, false, false, (livingEntity) ->
-                        (livingEntity instanceof Mob) && !(livingEntity instanceof PunishingBirdEntity)));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(
+                this, LivingEntity.class, 10, true, true, (livingEntity) ->
+                        (livingEntity instanceof Mob) && !(livingEntity instanceof PunishingBirdEntity) || livingEntity instanceof Player) {
+
+            @Override
+            protected AABB getTargetSearchArea(double pTargetDistance) {
+                return this.mob.getBoundingBox().inflate(pTargetDistance);
+            }
+
+        });
     }
 
     @Override
@@ -100,19 +120,11 @@ public class PunishingBirdEntity extends Monster implements IAnimatable {
     }
 
     private <E extends PunishingBirdEntity>PlayState predicate(final AnimationEvent<E> event) {
-        if (this.isAngry()) {
-            if (this.getAttackTick() > 0) {
-                event.getController().markNeedsReload();
-                event.getController().setAnimation(ATTACK_ANGRY);
-                return PlayState.CONTINUE;
-            }
+        if (this.getMaxHealth() == 1000.0F) {
             event.getController().setAnimation(FLY_ANGRY);
             return PlayState.CONTINUE;
-        } else if (this.swinging) {
-            event.getController().markNeedsReload();
-            event.getController().setAnimation(ATTACK_NORMAL);
-            return PlayState.CONTINUE;
-        } else if (event.isMoving()) {
+        }
+        if (event.isMoving()) {
             event.getController().setAnimation(FLY_NORMAL);
             return PlayState.CONTINUE;
         }
@@ -121,20 +133,14 @@ public class PunishingBirdEntity extends Monster implements IAnimatable {
     }
 
     private PlayState attackPredicate(AnimationEvent event) {
-//        if(event.getController().getAnimationState().equals(AnimationState.Stopped)) {
-//            event.getController().markNeedsReload();
-//            if (this.getAttackTick() > 0) {
-//                event.getController().setAnimation(ATTACK_ANGRY);
-//            } else if (this.swinging) {
-//                event.getController().setAnimation(ATTACK_NORMAL);
-//            }
-//            this.swinging = false;
-//        }
-//
-//        return PlayState.CONTINUE;
-        if (this.getAttackTick() > 0) {
+        if (this.getMaxHealth() == 1000.0F && this.swinging) {
             event.getController().markNeedsReload();
             event.getController().setAnimation(ATTACK_ANGRY);
+            this.swinging = false;
+        } else if (this.swinging) {
+            event.getController().markNeedsReload();
+            event.getController().setAnimation(ATTACK_NORMAL);
+            this.swinging = false;
         }
         return PlayState.CONTINUE;
     }
@@ -143,6 +149,8 @@ public class PunishingBirdEntity extends Monster implements IAnimatable {
     public void registerControllers(final AnimationData data) {
         data.addAnimationController(new AnimationController(this, "controller",
                 0, this::predicate));
+        data.addAnimationController(new AnimationController(this, "attack_controller",
+                0, this::attackPredicate));
     }
 
     @Override
@@ -154,7 +162,7 @@ public class PunishingBirdEntity extends Monster implements IAnimatable {
     protected void defineSynchedData() {
         super.defineSynchedData();
         this.getEntityData().define(IS_ANGRY, false);
-        this.getEntityData().define(ATTACK_TICK, 30);
+        this.getEntityData().define(ATTACK_TICK, 50);
     }
 
     public boolean isAngry() {
@@ -170,7 +178,19 @@ public class PunishingBirdEntity extends Monster implements IAnimatable {
     }
 
     public void startAttackAnim() {
-    this.getEntityData().set(ATTACK_TICK, 30);
+    this.getEntityData().set(ATTACK_TICK, 50);
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag pCompound) {
+        super.addAdditionalSaveData(pCompound);
+        pCompound.putBoolean("angry", isAngry());
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag pCompound) {
+        super.readAdditionalSaveData(pCompound);
+        setAngry(pCompound.getBoolean("angry"));
     }
 
     @Override
@@ -198,176 +218,27 @@ public class PunishingBirdEntity extends Monster implements IAnimatable {
         }
     }
 
-    public class PunishingBirdAngryAttackGoal extends Goal{//MeleeAttackGoalのコピペを少し改変
-        protected final PunishingBirdEntity mob;
-        private final double speedModifier;
-        private final boolean followingTargetEvenIfNotSeen;
-        private Path path;
-        private double pathedTargetX;
-        private double pathedTargetY;
-        private double pathedTargetZ;
-        private int ticksUntilNextPathRecalculation;
-        private int ticksUntilNextAttack;
-        private final int attackInterval = 20;
-        private long lastCanUseCheck;
-        private static final long COOLDOWN_BETWEEN_CAN_USE_CHECKS = 20L;
-        private int failedPathFindingPenalty = 0;
-        private boolean canPenalize = false;
+    public class PunishingBirdAngryAttackGoal extends MeleeAttackGoal {
+        protected PunishingBirdEntity owner;
 
         public PunishingBirdAngryAttackGoal(PunishingBirdEntity pMob, double pSpeedModifier, boolean pFollowingTargetEvenIfNotSeen) {
-            this.mob = pMob;
-            this.speedModifier = pSpeedModifier;
-            this.followingTargetEvenIfNotSeen = pFollowingTargetEvenIfNotSeen;
-            this.setFlags(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.LOOK));
+            super(pMob, pSpeedModifier, pFollowingTargetEvenIfNotSeen);
+            this.owner = pMob;
         }
 
+        @Override
         public boolean canUse() {
-            if (!this.mob.isAngry()) {
-                return false;
-            }
-            long i = this.mob.level.getGameTime();
-            if (i - this.lastCanUseCheck < 20L) {
-                return false;
-            } else {
-                this.lastCanUseCheck = i;
-                LivingEntity livingentity = this.mob.getTarget();
-                if (livingentity == null) {
-                    return false;
-                } else if (!livingentity.isAlive()) {
-                    return false;
-                } else {
-                    if (canPenalize) {
-                        if (--this.ticksUntilNextPathRecalculation <= 0) {
-                            this.path = this.mob.getNavigation().createPath(livingentity, 0);
-                            this.ticksUntilNextPathRecalculation = 4 + this.mob.getRandom().nextInt(7);
-                            return this.path != null;
-                        } else {
-                            return true;
-                        }
-                    }
-                    this.path = this.mob.getNavigation().createPath(livingentity, 0);
-                    if (this.path != null) {
-                        return true;
-                    } else {
-                        return this.getAttackReachSqr(livingentity) >= this.mob.distanceToSqr(livingentity.getX(), livingentity.getY(), livingentity.getZ());
-                    }
-                }
-            }
+            return super.canUse() && this.owner.isAngry();
         }
 
+        @Override
         public boolean canContinueToUse() {
-            if (!this.mob.isAngry()) {
-                return false;
-            }
-            LivingEntity livingentity = this.mob.getTarget();
-            if (livingentity == null) {
-                return false;
-            } else if (!livingentity.isAlive()) {
-                return false;
-            } else if (!this.followingTargetEvenIfNotSeen) {
-                return !this.mob.getNavigation().isDone();
-            } else if (!this.mob.isWithinRestriction(livingentity.blockPosition())) {
-                return false;
-            } else {
-                return !(livingentity instanceof Player) || !livingentity.isSpectator() && !((Player)livingentity).isCreative();
-            }
+            return super.canContinueToUse() && this.owner.isAngry();
         }
 
-        public void start() {
-            this.mob.getNavigation().moveTo(this.path, this.speedModifier);
-            this.mob.setAggressive(true);
-            this.ticksUntilNextPathRecalculation = 0;
-            this.ticksUntilNextAttack = 0;
-        }
-
-        public void stop() {
-            LivingEntity livingentity = this.mob.getTarget();
-            if (!EntitySelector.NO_CREATIVE_OR_SPECTATOR.test(livingentity)) {
-                this.mob.setTarget((LivingEntity)null);
-            }
-
-            this.mob.setAggressive(false);
-            this.mob.getNavigation().stop();
-        }
-
-        public boolean requiresUpdateEveryTick() {
-            return true;
-        }
-
-        public void tick() {
-            LivingEntity livingentity = this.mob.getTarget();
-            if (livingentity != null) {
-                this.mob.getLookControl().setLookAt(livingentity, 30.0F, 30.0F);
-                double d0 = this.mob.distanceToSqr(livingentity.getX(), livingentity.getY(), livingentity.getZ());
-                this.ticksUntilNextPathRecalculation = Math.max(this.ticksUntilNextPathRecalculation - 1, 0);
-                if ((this.followingTargetEvenIfNotSeen || this.mob.getSensing().hasLineOfSight(livingentity)) && this.ticksUntilNextPathRecalculation <= 0 && (this.pathedTargetX == 0.0D && this.pathedTargetY == 0.0D && this.pathedTargetZ == 0.0D || livingentity.distanceToSqr(this.pathedTargetX, this.pathedTargetY, this.pathedTargetZ) >= 1.0D || this.mob.getRandom().nextFloat() < 0.05F)) {
-                    this.pathedTargetX = livingentity.getX();
-                    this.pathedTargetY = livingentity.getY();
-                    this.pathedTargetZ = livingentity.getZ();
-                    this.ticksUntilNextPathRecalculation = 4 + this.mob.getRandom().nextInt(7);
-                    if (this.canPenalize) {
-                        this.ticksUntilNextPathRecalculation += failedPathFindingPenalty;
-                        if (this.mob.getNavigation().getPath() != null) {
-                            net.minecraft.world.level.pathfinder.Node finalPathPoint = this.mob.getNavigation().getPath().getEndNode();
-                            if (finalPathPoint != null && livingentity.distanceToSqr(finalPathPoint.x, finalPathPoint.y, finalPathPoint.z) < 1)
-                                failedPathFindingPenalty = 0;
-                            else
-                                failedPathFindingPenalty += 10;
-                        } else {
-                            failedPathFindingPenalty += 10;
-                        }
-                    }
-                    if (d0 > 1024.0D) {
-                        this.ticksUntilNextPathRecalculation += 10;
-                    } else if (d0 > 256.0D) {
-                        this.ticksUntilNextPathRecalculation += 5;
-                    }
-
-                    if (!this.mob.getNavigation().moveTo(livingentity, this.speedModifier)) {
-                        this.ticksUntilNextPathRecalculation += 15;
-                    }
-
-                    this.ticksUntilNextPathRecalculation = this.adjustedTickDelay(this.ticksUntilNextPathRecalculation);
-                }
-
-                this.ticksUntilNextAttack = Math.max(this.ticksUntilNextAttack - 1, 0);
-                this.checkAndPerformAttack(livingentity, d0);
-            }
-        }
-
-        protected void checkAndPerformAttack(LivingEntity pEnemy, double pDistToEnemySqr) {
-            double d0 = this.getAttackReachSqr(pEnemy);
-            if (pDistToEnemySqr <= d0 && this.ticksUntilNextAttack <= 0) {
-                if (this.mob.getAttackTick() == 0) {
-                    this.mob.startAttackAnim();
-                }
-                if (this.mob.getAttackTick() < 5) {
-                    this.resetAttackCooldown();
-                    this.mob.swing(InteractionHand.MAIN_HAND);
-                    this.mob.doHurtTarget(pEnemy);
-                }
-            }
-
-        }
-
-        protected void resetAttackCooldown() {
-            this.ticksUntilNextAttack = this.adjustedTickDelay(60);
-        }
-
-        protected boolean isTimeToAttack() {
-            return this.ticksUntilNextAttack <= 0;
-        }
-
-        protected int getTicksUntilNextAttack() {
-            return this.ticksUntilNextAttack;
-        }
-
-        protected int getAttackInterval() {
-            return this.adjustedTickDelay(60);
-        }
-
+        @Override
         protected double getAttackReachSqr(LivingEntity pAttackTarget) {
-            return (double)(this.mob.getBbWidth() * 2.0F * this.mob.getBbWidth() * 2.0F + pAttackTarget.getBbWidth()) + 32.0D;
+            return super.getAttackReachSqr(pAttackTarget) + 48.0D;
         }
     }
 }

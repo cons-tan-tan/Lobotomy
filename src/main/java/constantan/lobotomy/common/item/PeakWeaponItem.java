ef31ff7732fb8c3694e363c24614f285d9a03f52
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-07-10 05:09:50 +0900

    fix PeakWeaponItem

--- src/main/java/constantan/lobotomy/common/item/PeakWeaponItem.java
+++ src/main/java/constantan/lobotomy/common/item/PeakWeaponItem.java
@@ -1,7 +1,12 @@
 package constantan.lobotomy.common.item;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ProjectileWeaponItem;
+import net.minecraft.world.level.Level;
+import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -16,12 +21,17 @@ import software.bernie.geckolib3.util.GeckoLibUtil;
 import java.util.function.Predicate;
 
 public class PeakWeaponItem extends ProjectileWeaponItem implements IAnimatable, ISyncable {
-    private static final AnimationBuilder IDLE = new AnimationBuilder()
+
+    protected static final AnimationBuilder IDLE = new AnimationBuilder()
             .addAnimation("animation.peak_weapon.idle", ILoopType.EDefaultLoopTypes.HOLD_ON_LAST_FRAME);
     protected static final AnimationBuilder FIRE = new AnimationBuilder()
             .addAnimation("animation.peak_weapon.fire", ILoopType.EDefaultLoopTypes.PLAY_ONCE);
 
-    public final AnimationFactory factory = GeckoLibUtil.createFactory(this);
+    private final AnimationFactory FACTORY = GeckoLibUtil.createFactory(this);
+
+    public final int FIRE_ANIM_STATE = 0;
+
+    public final String ANIM_CONTROLLER_NAME = "controller";
 
     public PeakWeaponItem(Properties pProperties) {
         super(pProperties);
@@ -34,18 +44,27 @@ public class PeakWeaponItem extends ProjectileWeaponItem implements IAnimatable,
 
     @Override
     public void registerControllers(AnimationData data) {
-        data.addAnimationController(new AnimationController(this, "controller",
-                0, this::predicate));
+        data.addAnimationController(new AnimationController(this, ANIM_CONTROLLER_NAME, 0, this::predicate));
     }
 
     @Override
     public AnimationFactory getFactory() {
-        return this.factory;
+        return this.FACTORY;
     }
 
     @Override
     public void onAnimationSync(int id, int state) {
+        if (state == FIRE_ANIM_STATE) {
+            AnimationController controller = GeckoLibUtil.getControllerForID(this.FACTORY, id, ANIM_CONTROLLER_NAME);
+            if (controller.getAnimationState() == AnimationState.Stopped) {
 
+            }
+        }
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        return super.use(pLevel, pPlayer, pUsedHand);
     }
 
     @Override

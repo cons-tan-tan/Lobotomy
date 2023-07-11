06a63d20445ada8898bfe815fc93e64e8cbc3cc7
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-07-11 12:27:58 +0900

    fix PeakWeaponItem

--- src/main/java/constantan/lobotomy/common/item/PeakWeaponItem.java
+++ src/main/java/constantan/lobotomy/common/item/PeakWeaponItem.java
@@ -1,11 +1,13 @@
 package constantan.lobotomy.common.item;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ProjectileWeaponItem;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.network.PacketDistributor;
 import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -15,6 +17,7 @@ import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
+import software.bernie.geckolib3.network.GeckoLibNetwork;
 import software.bernie.geckolib3.network.ISyncable;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 
@@ -35,6 +38,7 @@ public class PeakWeaponItem extends ProjectileWeaponItem implements IAnimatable,
 
     public PeakWeaponItem(Properties pProperties) {
         super(pProperties);
+        GeckoLibNetwork.registerSyncable(this);
     }
 
     private PlayState predicate(AnimationEvent event) {
@@ -57,14 +61,20 @@ public class PeakWeaponItem extends ProjectileWeaponItem implements IAnimatable,
         if (state == FIRE_ANIM_STATE) {
             AnimationController controller = GeckoLibUtil.getControllerForID(this.FACTORY, id, ANIM_CONTROLLER_NAME);
             if (controller.getAnimationState() == AnimationState.Stopped) {
-
+                controller.markNeedsReload();
+                controller.setAnimation(FIRE);
             }
         }
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        return super.use(pLevel, pPlayer, pUsedHand);
+        ItemStack stack = pPlayer.getItemInHand(pUsedHand);
+        if (!pLevel.isClientSide) {
+            int id = GeckoLibUtil.guaranteeIDForStack(stack, (ServerLevel) pLevel);
+            GeckoLibNetwork.syncAnimation(PacketDistributor.TRACKING_ENTITY_AND_SELF.with(() -> pPlayer), this, id, FIRE_ANIM_STATE);
+        }
+        return InteractionResultHolder.consume(stack);
     }
 
     @Override

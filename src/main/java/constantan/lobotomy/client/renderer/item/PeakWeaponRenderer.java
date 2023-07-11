64e1b68f816025aa2cb67ca5308e771e2b58d5ab
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-07-11 12:28:50 +0900

    add PeakWeaponRenderer

---
+++ src/main/java/constantan/lobotomy/client/renderer/item/PeakWeaponRenderer.java
@@ -0,0 +1,7 @@
+package constantan.lobotomy.client.renderer.item;
+
+import constantan.lobotomy.common.item.PeakWeaponItem;
+import software.bernie.geckolib3.renderers.geo.GeoItemRenderer;
+
+public class PeakWeaponRenderer extends GeoItemRenderer<PeakWeaponItem> {
+}

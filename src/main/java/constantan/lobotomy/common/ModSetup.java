f7b4b74be05ae9611a888af7fb08607eaa686ae4
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-07-14 16:09:12 +0900

    add ModSetup

---
+++ src/main/java/constantan/lobotomy/common/ModSetup.java
@@ -0,0 +1,37 @@
+package constantan.lobotomy.common;
+
+import constantan.lobotomy.common.entity.PunishingBirdEntity;
+import constantan.lobotomy.common.init.ModEntityTypes;
+import constantan.lobotomy.common.init.ModItems;
+import constantan.lobotomy.lib.LibMisc;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import javax.annotation.Nonnull;
+
+@Mod.EventBusSubscriber(modid = LibMisc.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModSetup {
+
+    public static final CreativeModeTab CREATIVE_TAB = new CreativeModeTab(LibMisc.MOD_ID)
+    {
+        @Nonnull
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.GIANT_TREE_SAP.get());
+        }
+    };
+
+    public static void init(final FMLCommonSetupEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
+        event.put(ModEntityTypes.PUNISHING_BIRD.get(), PunishingBirdEntity.setAttributes());
+    }
+}

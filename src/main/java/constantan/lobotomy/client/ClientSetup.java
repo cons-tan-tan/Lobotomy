9ae4903bdccb6bde3184dc9e7ddf119b90882944
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-07-14 16:10:03 +0900

    add KeyBindings

--- src/main/java/constantan/lobotomy/client/ClientSetup.java
+++ src/main/java/constantan/lobotomy/client/ClientSetup.java
@@ -1,14 +1,23 @@
 package constantan.lobotomy.client;
 
+import constantan.lobotomy.client.key.KeyBindings;
+import constantan.lobotomy.client.key.KeyInputHandler;
 import constantan.lobotomy.client.renderer.entity.PunishingBirdRenderer;
 import constantan.lobotomy.common.init.ModEntityTypes;
+import constantan.lobotomy.lib.LibMisc;
 import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+@Mod.EventBusSubscriber(modid = LibMisc.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientSetup {
+
     public static void init(final FMLClientSetupEvent event) {
         EntityRenderers.register(ModEntityTypes.PUNISHING_BIRD.get(), PunishingBirdRenderer::new);
 
+        MinecraftForge.EVENT_BUS.addListener(KeyInputHandler::onKeyInput);
         KeyBindings.init();
     }
 }

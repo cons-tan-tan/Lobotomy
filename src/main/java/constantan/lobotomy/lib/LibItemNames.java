35928dd2cb3e4e7a1eab8360e226a62a0059b774
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-07-26 01:51:18 +0900

    remove spawn_egg name

--- src/main/java/constantan/lobotomy/lib/LibItemNames.java
+++ src/main/java/constantan/lobotomy/lib/LibItemNames.java
@@ -4,7 +4,5 @@ public class LibItemNames {
 
     public static final String GIANT_TREE_SAP = "giant_tree_sap";
 
-    public static final String PUNISHING_BIRD_SPAWN_EGG = "punishing_bird_spawn_egg";
-
     public static final String PEAK_WEAPON = "peak_weapon";
 }

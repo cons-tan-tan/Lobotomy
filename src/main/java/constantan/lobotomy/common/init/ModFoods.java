eb3a98b754ba84fe0804e172cfe8fcdcc6841e43
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-07-26 01:44:37 +0900

    remove

--- src/main/java/constantan/lobotomy/common/init/ModFoods.java
+++
@@ -1,8 +0,0 @@
-package constantan.lobotomy.common.init;
-
-import net.minecraft.world.food.FoodProperties;
-
-public class ModFoods {
-    public static final FoodProperties GIANT_TREE_SAP = (new FoodProperties.Builder())
-            .alwaysEat().build();
-}

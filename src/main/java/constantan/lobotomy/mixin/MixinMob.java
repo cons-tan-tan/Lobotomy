70a3f37a13f95dc7f42fc906e53363645080ace8
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-07-28 03:17:29 +0900

    mixin doHurtTarget method

---
+++ src/main/java/constantan/lobotomy/mixin/MixinMob.java
@@ -0,0 +1,20 @@
+package constantan.lobotomy.mixin;
+
+import constantan.lobotomy.common.entity.AbnormalityEntity;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Mob;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(Mob.class)
+public abstract class MixinMob {
+    @ModifyArg(method = "doHurtTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", ordinal = 0))
+    private DamageSource doHurtTarget(DamageSource pSource) {
+        Mob self = (Mob) (Object) this;
+        if (self instanceof AbnormalityEntity abnormality) {
+            return abnormality.getAbnormalDamageSource(abnormality.getDefaultDamageType(), true);//引数を弄れるようにする
+        }
+        return pSource;
+    }
+}

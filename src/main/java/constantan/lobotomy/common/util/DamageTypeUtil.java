fc1c31b8e10f7f0ec7fc0a8b97f686045faea623
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-07-28 03:22:53 +0900

    remove old method

--- src/main/java/constantan/lobotomy/common/util/DamageTypeUtil.java
+++ src/main/java/constantan/lobotomy/common/util/DamageTypeUtil.java
@@ -1,21 +1,8 @@
 package constantan.lobotomy.common.util;
 
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.LivingEntity;
-
 public enum DamageTypeUtil {
     RED,
     WHITE,
     BLACK,
     PALE;
-
-    public DamageSource getEntityDamageSource(LivingEntity livingEntity) {
-        DamageSource entityDamageSource = DamageSource.mobAttack(livingEntity);
-        return switch (this) {
-            case RED -> entityDamageSource;
-            case WHITE -> entityDamageSource.bypassArmor();
-            case BLACK -> entityDamageSource.bypassMagic();
-            case PALE -> entityDamageSource.bypassArmor().bypassMagic();
-        };
-    }
 }

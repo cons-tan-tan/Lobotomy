f6ade99fedfc4577def30962f284e91fa40fb4d5
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-07-29 02:30:35 +0900

    fix import

--- src/main/java/constantan/lobotomy/client/renderer/ModEntityRenderers.java
+++ src/main/java/constantan/lobotomy/client/renderer/ModEntityRenderers.java
@@ -2,7 +2,6 @@ package constantan.lobotomy.client.renderer;
 
 import constantan.lobotomy.client.renderer.entity.PunishingBirdRenderer;
 import constantan.lobotomy.client.renderer.entity.TheBurrowingHeavenRenderer;
-import constantan.lobotomy.common.entity.TheBurrowingHeavenEntity;
 import constantan.lobotomy.common.init.ModEntityTypes;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 

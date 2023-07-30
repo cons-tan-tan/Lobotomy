f8cdfbb1158129a68f81057a6acd3d86de36984d
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-07-31 08:05:31 +0900

    fix

--- src/main/java/constantan/lobotomy/common/entity/AbnormalityEntity.java
+++ src/main/java/constantan/lobotomy/common/entity/AbnormalityEntity.java
@@ -5,6 +5,7 @@ import constantan.lobotomy.common.util.DamageTypeUtil;
 import constantan.lobotomy.common.util.IRiskLevel;
 import constantan.lobotomy.common.util.RiskLevelUtil;
 import constantan.lobotomy.common.util.mixin.IMixinDamageSource;
+import constantan.lobotomy.common.util.mixin.IMixinEntityType;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Monster;
@@ -19,6 +20,9 @@ public abstract class AbnormalityEntity extends Monster implements IRiskLevel {
 
     protected final AnimationFactory FACTORY;
 
+    private final IMixinEntityType<?> abnormalityType;
+    private final IMixinDamageSource abnormalDamageSource;
+
     protected DamageTypeUtil currentDamageType;
 
     public AbnormalityEntity(EntityType<? extends Monster> pEntityType, Level pLevel) {
@@ -28,6 +32,9 @@ public abstract class AbnormalityEntity extends Monster implements IRiskLevel {
                 ? GeckoLibUtil.createFactory(iAnimatable)
                 : null;
 
+        this.abnormalityType = ModEntityTypes.abnormalityEntityType(this.getType());
+        this.abnormalDamageSource = (IMixinDamageSource) (Object) DamageSource.mobAttack(this);
+
         this.currentDamageType = this.getDefaultDamageType();
     }
 
@@ -36,23 +43,21 @@ public abstract class AbnormalityEntity extends Monster implements IRiskLevel {
         return this.FACTORY;
     }
 
-    @Override
-    public RiskLevelUtil getRiskLevel() {
-        return ModEntityTypes.abnormalityEntityType(this.getType()).getRiskLevel();
+    public DamageSource getAbnormalDamageSource(DamageTypeUtil damageType, boolean blockable) {
+        return (DamageSource) (Object) this.abnormalDamageSource.riskLevel(this.getRiskLevel()).damageType(damageType).Blockable(blockable);
     }
 
-    public DamageSource getAbnormalDamageSource(DamageTypeUtil damageType, boolean blockable) {
-        IMixinDamageSource damageSource = ((IMixinDamageSource) (Object) DamageSource.mobAttack(this))
-                .riskLevel(this.getRiskLevel()).damageType(this.getDefaultDamageType()).Blockable(blockable);
-        return (DamageSource) (Object) damageSource;
+    @Override
+    public RiskLevelUtil getRiskLevel() {
+        return this.abnormalityType.getRiskLevel();
     }
 
     public Map<DamageTypeUtil, Float> getDefense() {
-        return ModEntityTypes.abnormalityEntityType(this.getType()).getDefense();
+        return this.abnormalityType.getDefense();
     }
 
     public DamageTypeUtil getDefaultDamageType() {
-        return ModEntityTypes.abnormalityEntityType(this.getType()).getDamageType();
+        return this.abnormalityType.getDamageType();
     }
 
     public DamageTypeUtil getCurrentDamageType() {

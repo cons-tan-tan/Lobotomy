b5a52f8344a78012f664d602ab6439e2fc23f8bf
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-08-01 05:11:09 +0900

    add getFactory method

--- src/main/java/constantan/lobotomy/common/item/AbnormalityToolItem.java
+++ src/main/java/constantan/lobotomy/common/item/AbnormalityToolItem.java
@@ -3,16 +3,28 @@ package constantan.lobotomy.common.item;
 import constantan.lobotomy.common.util.IAbnormalityTool;
 import constantan.lobotomy.common.util.RiskLevelUtil;
 import net.minecraft.world.item.Item;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
+import software.bernie.geckolib3.util.GeckoLibUtil;
 
 public abstract class AbnormalityToolItem extends Item implements IAbnormalityTool {
 
+    public final AnimationFactory FACTORY;
     public final RiskLevelUtil riskLevel;
 
     public AbnormalityToolItem(Item.Properties pProperties) {
         super(pProperties);
 
+        this.FACTORY = this instanceof IAnimatable iAnimatable
+                ? GeckoLibUtil.createFactory(iAnimatable)
+                : null;
+
         AbnormalityToolItemProperties abnormalityToolItemProperties = (AbnormalityToolItemProperties) pProperties;
         this.riskLevel = abnormalityToolItemProperties.riskLevel;
+    }
+
+    public AnimationFactory getFactory() {
+        return this.FACTORY;
     }
 
     @Override

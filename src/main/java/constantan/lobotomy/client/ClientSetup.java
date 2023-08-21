570f9e6889af0bc92af44558a6de79c5a5b2ec15
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-08-21 22:29:45 +0900

    replace layer-adding from mixin to event

--- src/main/java/constantan/lobotomy/client/ClientSetup.java
+++ src/main/java/constantan/lobotomy/client/ClientSetup.java
@@ -3,13 +3,23 @@ package constantan.lobotomy.client;
 import constantan.lobotomy.client.gui.SanityHudOverlay;
 import constantan.lobotomy.client.key.KeyBindings;
 import constantan.lobotomy.client.key.KeyInputHandler;
+import constantan.lobotomy.client.layer.EgoSuitLayer;
 import constantan.lobotomy.client.renderer.ModArmorRenderers;
 import constantan.lobotomy.client.renderer.ModEntityRenderers;
 import constantan.lobotomy.lib.LibMisc;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.renderer.entity.ArmorStandRenderer;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.gui.ForgeIngameGui;
 import net.minecraftforge.client.gui.OverlayRegistry;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
@@ -23,5 +33,23 @@ public class ClientSetup {
         MinecraftForge.EVENT_BUS.addListener(KeyInputHandler::onKeyInput);
         KeyBindings.init();
         OverlayRegistry.registerOverlayAbove(ForgeIngameGui.HOTBAR_ELEMENT, "sanity", SanityHudOverlay.HUD_SANITY);
+    }
+
+    @SubscribeEvent
+    public static void addLayers(EntityRenderersEvent.AddLayers event) {
+        addPlayerLayer(event, "slim");
+        addPlayerLayer(event, "default");
+        LivingEntityRenderer renderer = event.getRenderer(EntityType.ARMOR_STAND);
+        if (renderer instanceof ArmorStandRenderer) {
+            renderer.addLayer(new EgoSuitLayer<>(renderer, new PlayerModel<>(event.getEntityModels().bakeLayer(ModelLayers.PLAYER), false)));
+        }
+    }
+
+    private static void addPlayerLayer(EntityRenderersEvent.AddLayers event, String skin) {
+        EntityRenderer<? extends Player> renderer = event.getSkin(skin);
+        if (renderer instanceof LivingEntityRenderer livingRenderer) {
+            boolean isSlim = skin.equals("slim");
+            livingRenderer.addLayer(new EgoSuitLayer(livingRenderer, new PlayerModel<>(event.getEntityModels().bakeLayer(isSlim ? ModelLayers.PLAYER_SLIM : ModelLayers.PLAYER), isSlim)));
+        }
     }
 }

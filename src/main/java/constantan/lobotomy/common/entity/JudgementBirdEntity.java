8c6d9a9a327d71a9516fd4f9008b73924f70b4ee
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-08-25 19:03:48 +0900

    fix to brain system

--- src/main/java/constantan/lobotomy/common/entity/JudgementBirdEntity.java
+++ src/main/java/constantan/lobotomy/common/entity/JudgementBirdEntity.java
@@ -1,51 +1,116 @@
 package constantan.lobotomy.common.entity;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.control.LookControl;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
-import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
-import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.behavior.EntityTracker;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.tslat.smartbrainlib.api.SmartBrainOwner;
+import net.tslat.smartbrainlib.api.core.BrainActivityGroup;
+import net.tslat.smartbrainlib.api.core.SmartBrainProvider;
+import net.tslat.smartbrainlib.api.core.behaviour.FirstApplicableBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.OneRandomBehaviour;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.attack.AnimatableMeleeAttack;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.look.LookAtTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.misc.Idle;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.move.MoveToWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetRandomWalkTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetWalkTargetToAttackTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.InvalidateAttackTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetPlayerLookTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.SetRandomLookTarget;
+import net.tslat.smartbrainlib.api.core.behaviour.custom.target.TargetOrRetaliate;
+import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.HurtBySensor;
+import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearbyPlayersSensor;
+import net.tslat.smartbrainlib.util.BrainUtils;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.controller.AnimationController;
-import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 
-import java.util.Random;
+import java.util.List;
 
-public class JudgementBirdEntity extends AbnormalityEntity implements IAnimatable {
+public class JudgementBirdEntity extends AbnormalityEntity implements IAnimatable, SmartBrainOwner<JudgementBirdEntity> {
 
     private static final int ATTACK_DAMAGE_RANDOM_RANGE = 10;
 
     public JudgementBirdEntity(EntityType<? extends Monster> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
-        this.lookControl = new JudgeMentBirdLookControl(this);
     }
 
     @Override
     public void registerControllers(AnimationData data) {
-        data.addAnimationController(new AnimationController<>(this, "body_controller",
-                0, this::predicate));
+        data.addAnimationController(new AnimationController<>(this, "body_controller", 0, event -> {
+            if (this.swinging) {
+                event.getController().setAnimation(ANIM_ATTACK);
+            } else if (this.isOnGround() && event.isMoving()) {
+                event.getController().setAnimation(ANIM_WALK);
+            } else {
+                event.getController().setAnimation(ANIM_IDLE);
+            }
+            return PlayState.CONTINUE;
+        }));
     }
 
-    private PlayState predicate(AnimationEvent<JudgementBirdEntity> event) {
-        if (this.swinging) {
-            event.getController().setAnimation(ANIM_ATTACK);
-        } else if (this.isOnGround() && event.isMoving()) {
-            event.getController().setAnimation(ANIM_WALK);
-        } else {
-            event.getController().setAnimation(ANIM_IDLE);
-        }
-        return PlayState.CONTINUE;
+    @Override
+    protected Brain.Provider<?> brainProvider() {
+        return new SmartBrainProvider<>(this);
+    }
+
+    @Override
+    protected void customServerAiStep() {
+        this.tickBrain(this);
+    }
+
+    @Override
+    public List<ExtendedSensor<JudgementBirdEntity>> getSensors() {
+        return ObjectArrayList.of(
+                new NearbyPlayersSensor<>(),
+                new HurtBySensor<>()
+        );
+    }
+
+    @Override
+    public BrainActivityGroup<JudgementBirdEntity> getCoreTasks() {
+        return BrainActivityGroup.coreTasks(
+                new LookAtTarget<>(),
+                new MoveToWalkTarget<>()
+        );
+    }
+
+    @Override
+    public BrainActivityGroup<JudgementBirdEntity> getIdleTasks() {
+        return BrainActivityGroup.idleTasks(
+                new FirstApplicableBehaviour<JudgementBirdEntity>(//ここのジェネリクスは無いとコンパイル通らない
+                        new TargetOrRetaliate<>(),
+                        new SetPlayerLookTarget<>(){
+                            @Override
+                            protected void start(LivingEntity entity) {
+                                BrainUtils.setForgettableMemory(entity, MemoryModuleType.LOOK_TARGET, new EntityTracker(this.target, true),
+                                        entity.getRandom().nextInt(50, 100));
+                            }
+                        }.predicate(player -> player.distanceTo(this) < 4),
+                        new SetRandomLookTarget<>()
+                ), new OneRandomBehaviour<>(
+                        new SetRandomWalkTarget<>(),
+                        new Idle<>().runFor(livingEntity -> livingEntity.getRandom().nextInt(200, 600))
+                ));
+    }
+
+    @Override
+    public BrainActivityGroup<JudgementBirdEntity> getFightTasks() {
+        return BrainActivityGroup.fightTasks(
+                new InvalidateAttackTarget<>(),
+                new SetWalkTargetToAttackTarget<>(),
+                new AnimatableMeleeAttack<>(40)
+        );
     }
 
     public static AttributeSupplier setAttributes() {
@@ -57,20 +122,9 @@ public class JudgementBirdEntity extends AbnormalityEntity implements IAnimatabl
     }
 
     @Override
-    protected void registerGoals() {
-        this.goalSelector.addGoal(1, new FloatGoal(this));
-        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0F, false));
-        this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
-
-        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(
-                this, LivingEntity.class, 10, true, true, livingEntity ->
-                livingEntity instanceof Player));
-    }
-
-    @Override
     public double getAttributeValue(Attribute pAttribute) {
         if (pAttribute == Attributes.ATTACK_DAMAGE) {
-            return super.getAttributeValue(pAttribute) + new Random().nextInt(ATTACK_DAMAGE_RANDOM_RANGE + 1);
+            return super.getAttributeValue(pAttribute) + this.getRandom().nextInt(ATTACK_DAMAGE_RANDOM_RANGE + 1);
         }
         return super.getAttributeValue(pAttribute);
     }
@@ -83,18 +137,5 @@ public class JudgementBirdEntity extends AbnormalityEntity implements IAnimatabl
     @Override
     public boolean canDoKnockbackAttack() {
         return false;
-    }
-
-    private static class JudgeMentBirdLookControl extends LookControl {
-
-        public JudgeMentBirdLookControl(Mob pMob) {
-            super(pMob);
-        }
-
-        @Override
-        public void setLookAt(double pX, double pY, double pZ, float pDeltaYaw, float pDeltaPitch) {
-            super.setLookAt(pX, pY, pZ, pDeltaYaw, pDeltaPitch);
-            this.yMaxRotSpeed = Math.min(this.yMaxRotSpeed, 100.0F);
-        }
     }
 }

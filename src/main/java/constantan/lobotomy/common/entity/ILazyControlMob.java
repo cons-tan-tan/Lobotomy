76de1ec19e6d930320a771c9d995d54d0e916fce
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-08-27 08:20:51 +0900

    add lazy controller mob

---
+++ src/main/java/constantan/lobotomy/common/entity/ILazyControlMob.java
@@ -0,0 +1,38 @@
+package constantan.lobotomy.common.entity;
+
+import constantan.lobotomy.common.entity.ai.control.LazyBodyRotationControl;
+import constantan.lobotomy.common.entity.ai.control.LazyLookControl;
+import constantan.lobotomy.common.entity.ai.control.LazyMoveControl;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.control.BodyRotationControl;
+import net.minecraft.world.entity.ai.control.LookControl;
+import net.minecraft.world.entity.ai.control.MoveControl;
+import net.minecraftforge.common.util.Lazy;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Predicate;
+
+public interface ILazyControlMob<P extends Mob> {
+
+    private P self() {
+        return (P) this;
+    }
+
+    default LookControl createLookControl() {
+        return new LazyLookControl<>(self())
+                .isLazyIf(this.getPredicateLazy());
+    }
+
+    default MoveControl createMoveControl() {
+        return new LazyMoveControl<>(self())
+                .isLazyIf(this.getPredicateLazy());
+    }
+
+    @NotNull
+    default BodyRotationControl createBodyControl() {
+        return new LazyBodyRotationControl<>(self())
+                .isLazyIf(this.getPredicateLazy());
+    }
+
+    Lazy<Predicate<P>> getPredicateLazy();
+}

007681c58544c1f10582b3addc36efdd3284ca48
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-09-08 10:11:40 +0900

    enable right click equip

--- src/main/java/constantan/lobotomy/common/item/EgoGift.java
+++ src/main/java/constantan/lobotomy/common/item/EgoGift.java
@@ -2,11 +2,18 @@ package constantan.lobotomy.common.item;
 
 import constantan.lobotomy.common.ModSetup;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 public class EgoGift extends Item implements ICurioItem {
 
     public EgoGift(Properties pProperties) {
         super(pProperties.stacksTo(1).tab(ModSetup.CREATIVE_TAB));
+    }
+
+    @Override
+    public boolean canEquipFromUse(SlotContext slotContext, ItemStack stack) {
+        return true;
     }
 }
